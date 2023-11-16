f1012613b007ee1a9dce4762b71d40f0b4635505
co2e14 <christian.orr@diamond.ac.uk>
2023-11-16 10:14:45 +0000

    update control.py locations in scripts

--- testing/moveonclick_tk.py
+++ testing/moveonclick_tk.py
@@ -7,7 +7,7 @@ import os
 import datetime
 import subprocess
 import pv
-from bin.control import ca
+from emerita.control import ca
 import os, re, sys
 import math, time, string, inspect
 from datetime import datetime
